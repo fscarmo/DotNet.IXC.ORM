87b7249321118eaefce9c959c32a6e5ce37b3eb8
Felipe S. Carmo <flpssdocarmo@gmail.com>
2026-04-24 21:17:50 -0300

    Teste de envio de requisição :white_check_mark:

---
+++ DotNet.IXC.ORM.Test/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DotNet.IXC.ORM.Test.Models;
+
+
+public class Client
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("razao")]
+    public string Razao { get; set; } = string.Empty;
+}
---
+++ DotNet.IXC.ORM.Test/RequestTest.cs
@@ -0,0 +1,23 @@
+﻿using DotNet.IXC.ORM.Test.Models;
+
+
+namespace DotNet.IXC.ORM.Test;
+
+
+public class RequestTest
+{
+    public RequestTest()
+    {
+        Utils.BuildHost();
+    }
+
+
+    [Fact]
+    public async Task ListClients()
+    {
+        var orm = new IxcOrm("cliente");
+        var content = await orm.Where("razao").Like("FELIPE DE SOUSA").GetAsync<Client>();
+
+        Assert.NotNull(content);
+    }
+}
