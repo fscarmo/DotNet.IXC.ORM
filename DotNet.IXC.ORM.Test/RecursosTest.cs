fa4c9b6209fbbc7ff7cd9c8dcab4ad47eeae0d01
Felipe S. Carmo <flpssdocarmo@gmail.com>
2026-04-27 17:44:46 -0300

    Teste de ativação de contrato

---
+++ DotNet.IXC.ORM.Test/RecursosTest.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNet.IXC.ORM.Test;
+
+
+public class RecursosTest
+{
+    public RecursosTest()
+    {
+        Utils.MockedHostBuilder().Build();
+    }
+
+
+    [Fact]
+    public async Task FalhaAoTentarAtivarContratoJaAtivoTest()
+    {
+        var response = await IxcRecurso.AtivarContrato(8466);
+
+        Assert.NotNull(response);
+        Assert.Equal("error", response.Type);
+        Assert.Contains("Não foi possível ativar o contrato ID: ", response.Message);
+    }
+}
