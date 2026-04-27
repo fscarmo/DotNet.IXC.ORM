da1725a99902781a4ce7eab7407c149c643306b3
Felipe S. Carmo <flpssdocarmo@gmail.com>
2026-04-27 17:45:02 -0300

    Teste de limpeza de MAC

--- DotNet.IXC.ORM.Test/RecursosTest.cs
+++ DotNet.IXC.ORM.Test/RecursosTest.cs
@@ -18,4 +18,15 @@ public class RecursosTest
         Assert.Equal("error", response.Type);
         Assert.Contains("Não foi possível ativar o contrato ID: ", response.Message);
     }
+
+
+    [Fact]
+    public async Task LimparMacTest()
+    {
+        var response = await IxcRecurso.LimparMac(9159);
+
+        Assert.NotNull(response);
+        Assert.Equal("success", response.Type);
+        Assert.Contains("MAC removido com sucesso", response.Message);
+    }
 }
