ca27506d2e319bde6f017df60407daecd80ea95e
Felipe S. Carmo <flpssdocarmo@gmail.com>
2026-04-28 12:22:30 -0300

    Documentação adicionada à classe IxcOrm

--- DotNet.IXC.ORM/IxcOrm.cs
+++ DotNet.IXC.ORM/IxcOrm.cs
@@ -6,6 +6,20 @@ using DotNet.IXC.ORM.Enums;
 namespace DotNet.IXC.ORM;
 
 
+/// <summary>
+/// A classe <c>IxcOrm</c> implementa métodos que geram uma query de busca e herda o comportamento da classe
+/// <see cref="RequestEmitter"/>, para disponibilizar, através da mesma instância, os métodos que executam requisições HTTP
+/// para a API do IXC Provedor.
+/// <para>
+/// Essa classe manipula as classes de ordenação, paginação e de construção de parâmetros, além de gerar um JSON compatível com
+/// a query de busca da API do IXC Provedor.
+/// </para>
+/// </summary>
+/// <remarks>
+/// Autor: Felipe S. Carmo <br/>
+/// Versão: 1.2.0 <br/>
+/// Desde: 2026-04-28
+/// </remarks>
 public class IxcOrm(string table) : RequestEmitter(table)
 {
     private readonly List<Parameter> parameters = [];
@@ -16,6 +30,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     private Parameter.Builder parameterBuilder = Parameter.NewBuilder(table);
 
 
+    /// <summary>
+    /// Compara a string "expected" com a query gerada pela classe, ignorando espaços em branco e quebras de linha.
+    /// Retorna true se as strings forem equivalentes, ou false caso contrário.
+    /// </summary>
+    /// <param name="expected">A string esperada para comparação com a query gerada.</param>
+    /// <returns>True se as strings forem equivalentes, ou false caso contrário.</returns>
     public bool ValidateQuery(string expected)
     {
         string normalizedExpected = expected
@@ -29,6 +49,11 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Define a paginação na query de consulta.
+    /// </summary>
+    /// <param name="pagination">Um objeto <see cref="Pagination"/> com as configurações de paginação.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm WithPagination(int page, int rows)
     {
         pagination = new Pagination(page, rows);
@@ -36,6 +61,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Define como a API do IXC Provedor deverá ordenar os dados retornados pela busca.
+    /// </summary>
+    /// <param name="sortName">O campo da tabela que será usado para ordenar os registros retornados.</param>
+    /// <param name="sortOrder">O tipo de ordenação (asc | desc).</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm OrderBy(string sortName, IxcOrmSort sortOrder)
     {
         ordering = (sortOrder == IxcOrmSort.Desc)
@@ -48,6 +79,11 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Inicia um novo objeto de parâmetro para a propriedade <c>grid_param</c> da query.
+    /// </summary>
+    /// <param name="column">O campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm Where(string column)
     {
         parameterBuilder.WithType(column);
@@ -55,6 +91,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (L) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm Like(object value)
     {
         parameterBuilder
@@ -67,6 +109,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (=) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm Exactly(object value)
     {
         parameterBuilder
@@ -79,6 +127,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (!=) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm Not(object value)
     {
         parameterBuilder
@@ -91,6 +145,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (&lt;) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm LessThan(object value)
     {
         parameterBuilder
@@ -103,6 +163,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (&lt;=) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm LessThanOrEqual(object value)
     {
         parameterBuilder
@@ -115,6 +181,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (&gt;) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm GreaterThan(object value)
     {
         parameterBuilder
@@ -127,6 +199,12 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
+    /// <summary>
+    /// Adiciona o operador de comparação (&gt;=) e o valor a ser filtrado no objeto de parâmetro iniciado por 
+    /// <see cref="Where(string)"/>.
+    /// </summary>
+    /// <param name="value">O valor do campo da tabela que será usado como filtro na busca.</param>
+    /// <returns>A própria instância de <see cref="IxcOrm"/>.</returns>
     public IxcOrm GreaterThanOrEqual(object value)
     {
         parameterBuilder
@@ -139,14 +217,17 @@ public class IxcOrm(string table) : RequestEmitter(table)
     }
 
 
-    private void PrepareQuery()
-    {
-        parameters.Add(parameterBuilder.Build());
-        parameterBuilder = Parameter.NewBuilder(Table);
-        SetupQuery(GetQueryAsJsonString());
-    }
-
-
+    /// <summary>
+    /// Sobrescreve a chamada para <see cref="RequestEmitter.EmitRequest(HttpMethod, CancellationToken?)"/>.
+    /// Envia a requisição para a API do IXC Provedor e retorna o conteúdo em uma string.
+    /// </summary>
+    /// <param name="method">O método HTTP (GET, POST, PUT, DELETE).</param>
+    /// <param name="cancellationToken">Token de cancelamento opcional.</param>
+    /// <returns>O conteúdo da resposta em uma string.</returns>
+    /// <exception cref="DotNet.IXC.ORM.Exceptions.IxcOrmRequestException">
+    /// Lançada quando ocorre uma falha na comunicação com o servidor, na requisição HTTP.
+    /// Ou quando a API responde com um status code diferente de 200 (OK).
+    /// </exception>
     protected override async Task<string> EmmitRequest(HttpMethod method, CancellationToken? cancellationToken = null)
     {
         string result = await base.EmmitRequest(method, cancellationToken);
@@ -156,6 +237,14 @@ public class IxcOrm(string table) : RequestEmitter(table)
         SetupQuery(string.Empty);
 
         return result;
+    }
+
+
+    private void PrepareQuery()
+    {
+        parameters.Add(parameterBuilder.Build());
+        parameterBuilder = Parameter.NewBuilder(Table);
+        SetupQuery(GetQueryAsJsonString());
     }
 
 
